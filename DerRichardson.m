3db7b3541bf117f65202d53955501e4d2279b52a
Alf-caput <alfre.roble.aba@gmail.com>
2023-04-29 11:05:11 +0200

    comentarios

--- DerRichardson.m
+++ DerRichardson.m
@@ -1,6 +1,7 @@
 function [dfdx, d2fdx] = DerRichardson(f, x0, hin, N)
-% Función que calcula la derivada primera y segunda de una función anónima
-% con el método de Richardson.
+% Función que calcula la derivada primera y segunda de una función en un 
+% punto.
+% El algoritmo utilizado es el método de Richardson.
 % Entradas:
 %   f = función dada en forma anónima (f = @(x))
 %   x0 = punto donde se calcula la derivada
@@ -10,20 +11,25 @@ function [dfdx, d2fdx] = DerRichardson(f, x0, hin, N)
 %   dfdx = matriz de extrapolación para la 1ª derivada
 %   d2fdx = matriz de extrapolación para la 2ª derivada
 
-    % Inicializar las matrices de extrapolación con ceros
+    % Se inicializan las matrices de extrapolación con ceros
     dfdx = zeros(N, N+1);
     d2fdx = zeros(N, N+1);
     
-    % Calcular las primeras columnas de las matrices de extrapolación
+    % Se calculan las primeras columnas de las matrices de extrapolación
     for i = 1: N
+        % Dimensión del intervalo (valores de paso)
         h = hin/2^(i-1);
+
         dfdx(i,1) = h;
+        % diferencias centrales de dos puntos para la primera derivada
+        dfdx(i,2) = (f(x0+h) - f(x0-h))/(2*h); % primeras extrapolaciones
+
         d2fdx(i,1) = h;
-        dfdx(i,2) = (f(x0+h) - f(x0-h))/(2*h); % diferencias centrales de dos puntos
-        d2fdx(i,2) = (f(x0-h) - 2*f(x0) + f(x0+h))/(h^2); % diferencias centrales de tres puntos
+        % diferencias centrales de tres puntos para la segunda derivada
+        d2fdx(i,2) = (f(x0-h) - 2*f(x0) + f(x0+h))/(h^2);% primeras extrapolaciones
     end
     
-    % Calcular las extrapolaciones
+    % Se calculan las siguientes extrapolaciones
     for j = 3: N+1
         for i = j-1:N
             dfdx(i,j) = dfdx(i,j-1) + (dfdx(i,j-1) - dfdx(i-1,j-1))/(4^(j-2)-1);

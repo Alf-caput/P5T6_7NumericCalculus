ee5dcb80113d07f269370867f4d5d596bb3246bf
Alf-caput <alfre.roble.aba@gmail.com>
2023-04-30 23:41:43 +0200

    trapecios comentario corregido

--- Trapeciosn.m
+++ Trapeciosn.m
@@ -3,13 +3,13 @@ function I = Trapeciosn(Fun,a,b,n)
 % compuesto de los trapecios 
 % Input:
 %   Fun = una función anónima de la forma Fun = @(x).
-%   a y b = límites de integración (a>b).
+%   a, b = límites de integración (a < b).
 %   n = número de subintervalos empleado. 
 % Output: 
 %   I = Valor de la integral
 
-h = (b-a)/n;  % Ancho del intervalo
-x = a:h:b;  % Puntos en el eje x
-y = Fun(x);  % Evalúa la función en los puntos del eje x
-I = h/2*(y(1) + 2*sum(y(2:end-1)) + y(end));  % Fórmula compuesta de los trapecios
+    h = (b-a)/n;  % Ancho del intervalo
+    x = a:h:b;  % Puntos en el eje x
+    y = Fun(x);  % Evalúa la función en los puntos del eje x
+    I = h/2*(y(1) + 2*sum(y(2:end-1)) + y(end));  % Fórmula compuesta de los trapecios
 end

bb30d143b93262ff0f38fc70044a77021523360c
Alf-caput <alfre.roble.aba@gmail.com>
2023-05-06 23:03:00 +0200

    names

--- PrmDerPar.m
+++ PrmDerPar.m
@@ -28,31 +28,31 @@ function [dfdx, dfdy] = PrmDerPar(x, y, f)
     dfdx = zeros(nx, ny);
     dfdy = zeros(nx, ny);
 
-    % Se calcula df/dy usando diferencia central de dos puntos en puntos interiores
+    % Se calcula df/dx usando diferencia central de dos puntos en puntos interiores
     for i = 2:nx-1
         for j = 1:ny
             dfdx(i,j) = (f(i+1,j) - f(i-1,j)) / (2*hx);
         end
     end
 
-    % Se calcula df/dy usando diferencia hacia delante y hacia atrás de 3
+    % Se calcula df/dx usando diferencia hacia delante y hacia atrás de 3
     % puntos en el primer y último punto respectivamente
     for j = 1:ny
         dfdx(1,j) = (-3*f(1,j) + 4*f(2,j) - f(3,j)) / (2*hx);
         dfdx(nx,j) = (f(nx-2,j) - 4*f(nx-1,j) + 3*f(nx,j)) / (2*hx);
     end
 
-    % Se calcula df/dx usando diferencia central de dos puntos en puntos interiores
+    % Se calcula df/dy usando diferencia central de dos puntos en puntos interiores
     for j = 2:ny-1
         for i = 1:nx
             dfdy(i,j) = (f(i,j+1) - f(i,j-1)) / (2*hy);
         end
     end
 
-    % Se calcula df/dx usando diferencia hacia delante y hacia atrás de 3
+    % Se calcula df/dy usando diferencia hacia delante y hacia atrás de 3
     % puntos en el primer y último punto respectivamente
     for i = 1:ny
         dfdy(i,1) = (-3*f(i,1) + 4*f(i,2) - f(i,3)) / (2*hy);
-        dfdy(i,ny) = (f(i,ny-2) - 4*f(i,ny-1) + 3*f(i,ny)) / (2*hx);
+        dfdy(i,ny) = (f(i,ny-2) - 4*f(i,ny-1) + 3*f(i,ny)) / (2*hy);
     end
 end

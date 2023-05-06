3e2a79fdebc71bf3cbb91193b00edf595a4006e1
Alf-caput <alfre.roble.aba@gmail.com>
2023-05-06 20:38:29 +0200

    Correcto creo

--- PrmDerPar.m
+++ PrmDerPar.m
@@ -1,4 +1,4 @@
-function [dfdx, dfdy] = PrmDerPar(x, y, f)
+function [dfdy, dfdx] = PrmDerPar(x, y, f)
 % Evalúa las primeras derivadas parciales df/dx y df/dy de una función f(x,y)
 % dada por puntos tabulados discretos con igual espaciado
 % INPUTS:
@@ -17,42 +17,41 @@ function [dfdx, dfdy] = PrmDerPar(x, y, f)
 % las comprobaciones pertinentes.
 
     % Tamaño de la matriz f
-    [ny, nx] = size(f);
+    [nx, ny] = size(f);
 
     % Espaciado entre valores
     hx = x(2) - x(1);
     hy = y(2) - y(1);
     
     % Se inicializan las matrices dfdx y dfdy con ceros
-    dfdx = zeros(ny, nx);
-    dfdy = zeros(ny, nx);
+    dfdy = zeros(nx, ny);
+    dfdx = zeros(nx, ny);
 
     % Se calcula df/dy usando diferencia central de dos puntos en puntos interiores
-    for i = 2:ny-1
-        for j = 1:nx
-            dfdy(i,j) = (f(i+1,j) - f(i-1,j)) / (2*hy);
+    for i = 2:nx-1
+        for j = 1:ny
+            dfdx(i,j) = (f(i+1,j) - f(i-1,j)) / (2*hy);
         end
     end
 
     % Se calcula df/dy usando diferencia hacia delante y hacia atrás de 3
     % puntos en el primer y último punto respectivamente
-    for j = 1:nx
-
-        dfdy(1,j) = (-3*f(1,j) + 4*f(2,j) - f(3,j)) / (2*hy);
-        dfdy(ny,j) = (f(ny-2,j) - 4*f(ny-1,j) + 3*f(ny,j)) / (2*hy);
+    for j = 1:ny
+        dfdx(1,j) = (-3*f(1,j) + 4*f(2,j) - f(3,j)) / (2*hy);
+        dfdx(nx,j) = (f(nx-2,j) - 4*f(nx-1,j) + 3*f(nx,j)) / (2*hy);
     end
 
     % Se calcula df/dx usando diferencia central de dos puntos en puntos interiores
-    for i = 2:nx-1
-        for j = 1:ny
-            dfdx(i,j) = (f(i,j+1) - f(i,j-1)) / (2*hx);
+    for i = 2:ny-1
+        for j = 1:nx
+            dfdy(i,j) = (f(i,j+1) - f(i,j-1)) / (2*hx);
         end
     end
 
     % Se calcula df/dx usando diferencia hacia delante y hacia atrás de 3
     % puntos en el primer y último punto respectivamente
-    for i = 1:nx
-        dfdx(i, 1) = (-3*f(i,1) + 4*f(i,2) - f(i,3)) / (2*hx);
-        dfdx(i,nx) = (f(i,nx-2) - 4*f(i,nx-1) + 3*f(i,nx)) / (2*hy);
+    for i = 1:ny
+        dfdy(i,1) = (-3*f(i,1) + 4*f(i,2) - f(i,3)) / (2*hx);
+        dfdy(i,ny) = (f(i,ny-2) - 4*f(i,ny-1) + 3*f(i,ny)) / (2*hy);
     end
 end

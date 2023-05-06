c7ef6750bad40e816f76c943883c438e3954f8be
Ana R <anrukiroble@gmail.com>
2023-05-06 14:29:55 +0200

    corregido el enunciado y añadido comentarios a la función

--- PrmDerPar.m
+++ PrmDerPar.m
@@ -11,14 +11,14 @@ function [dfdx, dfdy] = PrmDerPar(x, y, f)
 %   dfdx, dfdy = matrices con los valores de las derivadas parciales 
 %   en cada punto de modo que la posición (i,j) de la matriz de resultados 
 %   sea la derivada en el punto que corresponde a x(i), y(j)
-% Obteniendo las dimensiones de la matriz f
+    % Dimensiones de la matriz f
     [m, n] = size(f);
     
-    % Inicializando las matrices dfdx y dfdy con ceros
+    % Inicialización de variables
     dfdx = zeros(m, n);
     dfdy = zeros(m, n);
     
-    % Calculando las derivadas parciales con la fórmula de diferencia central de dos puntos en los puntos interiores
+    % Cálculo de las derivadas parciales con la fórmula de diferencia central de dos puntos en los puntos interiores
     for i = 2:m-1
         for j = 2:n-1
             dfdx(i, j) = (f(i+1, j) - f(i-1, j)) / (x(i+1) - x(i-1));
@@ -26,7 +26,7 @@ function [dfdx, dfdy] = PrmDerPar(x, y, f)
         end
     end
     
-    % Calculando las derivadas parciales con la fórmula de diferencia de tres puntos hacia delante y hacia atrás en los puntos extremos
+    % Cálculo de las derivadas parciales con la fórmula de diferencia de tres puntos hacia delante y hacia atrás en los puntos extremos
     dfdx(1,:) = (-3*f(1,:) + 4*f(2,:) - f(3,:)) / (x(2) - x(1));
     dfdx(m,:) = (3*f(m,:) - 4*f(m-1,:) + f(m-2,:)) / (x(m) - x(m-1));
     dfdy(:,1) = (-3*f(:,1) + 4*f(:,2) - f(:,3)) / (y(2) - y(1));
